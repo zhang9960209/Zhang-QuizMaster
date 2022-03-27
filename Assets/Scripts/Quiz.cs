c9eb41584cadc493887bd917519ef43188fbf111
LAPTOP-BKNJG3GA\13177 <zhang996@morrisville.edu>
2022-03-27 18:52:20 -0400

    Lists

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -7,9 +7,10 @@ using UnityEngine.UI;
 public class Quiz : MonoBehaviour
 {
     [Header("Questions")]
-    [SerializeField] QSOs Questions;
+    [SerializeField] List<QSOs> lsQuestions = new List<QSOs>();
     [SerializeField] TextMeshProUGUI qText;
-    
+    QSOs currentQuestion;
+
     [Header("Answers")]
     [SerializeField] GameObject[] ansButtons;
     int intCorrectAnsIndex;
@@ -27,9 +28,7 @@ public class Quiz : MonoBehaviour
     void Start()
     {
         timer = FindObjectOfType<Timer>();
-        NextQuestion();
-        //DisplayQuestion();
-        //DefaultButtonSprite();
+        
     }
 
     void Update()
@@ -59,7 +58,7 @@ public class Quiz : MonoBehaviour
     void DisplayAns(int intIndex)
     {
         Image buttonImage;
-        if (intIndex == Questions.GetCorrectAnsIndex())
+        if (intIndex == currentQuestion.GetCorrectAnsIndex())
         {
             qText.text = "Correct!";
             buttonImage = ansButtons[intIndex].GetComponent<Image>();
@@ -68,8 +67,8 @@ public class Quiz : MonoBehaviour
 
         else
         {
-            intCorrectAnsIndex = Questions.GetCorrectAnsIndex();
-            string strCorrectAns = Questions.GetAnswer(intCorrectAnsIndex);
+            intCorrectAnsIndex = currentQuestion.GetCorrectAnsIndex();
+            string strCorrectAns = currentQuestion.GetAnswer(intCorrectAnsIndex);
             qText.text = "Woops! The correct answer is: " + strCorrectAns;
             buttonImage = ansButtons[intCorrectAnsIndex].GetComponent<Image>();
             buttonImage.sprite = correctAnsSprite;
@@ -77,20 +76,36 @@ public class Quiz : MonoBehaviour
         }
     }
     void NextQuestion()
-    { 
-        ButtonState(true);
-        DefaultButtonSprite();
-        DisplayQuestion();
+    {
+        if (lsQuestions.Count > 0) 
+        {
+            ButtonState(true);
+            DefaultButtonSprite();
+            GetRandomQuestion();
+            DisplayQuestion();
+        }
+       
+       
     }
-    
+
+    void GetRandomQuestion()
+    {
+        int intIndex = Random.Range(0, lsQuestions.Count);
+        currentQuestion = lsQuestions[intIndex];
+        if (lsQuestions.Contains(currentQuestion))
+        {
+            lsQuestions.Remove(currentQuestion);
+        }
+        
+    }
     void DisplayQuestion()
     {
-        qText.text = Questions.GetQuestion();
+        qText.text = currentQuestion.GetQuestion();
 
         for (int i = 0; i < ansButtons.Length; i++)
         {
             TextMeshProUGUI bText = ansButtons[i].GetComponentInChildren<TextMeshProUGUI>();
-            bText.text = Questions.GetAnswer(i);
+            bText.text = currentQuestion.GetAnswer(i);
 
         }
     }
