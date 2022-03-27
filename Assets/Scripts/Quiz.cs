b9b3545e32651b448cb39f82e596c232225043c5
LAPTOP-BKNJG3GA\13177 <zhang996@morrisville.edu>
2022-03-27 13:54:35 -0400

    Swapping Sprites

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -2,14 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
     [SerializeField] QSOs Questions;
     [SerializeField] TextMeshProUGUI qText;
+    [SerializeField] GameObject[] ansButtons;
+
+    int intCorrectAnsIndex;
+    [SerializeField] Sprite defaultAnsSprite;
+    [SerializeField] Sprite correctAnsSprite;
+
     void Start()
     {
         qText.text = Questions.GetQuestion();
+
+        for (int i = 0; i < ansButtons.Length; i++)
+        {
+            TextMeshProUGUI bText = ansButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            bText.text = Questions.GetAnswer(i);
+
+        }
     }
 
+    public void OnAnsSelected(int intIndex)
+    {
+        Debug.Log("test");
+        Image buttonImage;
+        if (intIndex == Questions.GetCorrectAnsIndex())
+        {
+            qText.text = "Correct!";
+            buttonImage = ansButtons[intIndex].GetComponent<Image>();
+            buttonImage.sprite = correctAnsSprite;
+        }
+
+        else
+        { 
+            intCorrectAnsIndex = Questions.GetCorrectAnsIndex();
+            string strCorrectAns = Questions.GetAnswer(intCorrectAnsIndex);
+            qText.text = "Woops! The correct answer is \n" + strCorrectAns;
+            buttonImage = ansButtons[intCorrectAnsIndex].GetComponent<Image>();
+            buttonImage.sprite = correctAnsSprite;
+
+        }
+            
+    }
 }
