fa32cd7c56d6da39142610d5a3a1790b041464c7
LAPTOP-BKNJG3GA\13177 <zhang996@morrisville.edu>
2022-03-27 22:49:03 -0400

    End Screen

---
+++ Assets/Scripts/FinalScene.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FinalScene : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI finalTxt;
+    Score scoreKeeper;
+
+    void Start()
+    {
+        scoreKeeper = FindObjectOfType<Score>();
+    }
+
+    public void ShowFinalTxt()
+    {
+        finalTxt.text = "Thanks for taking the quiz. \nYou scored " +
+                        scoreKeeper.ScoreCalculator() + "% accuracy."; 
+    }
+}
